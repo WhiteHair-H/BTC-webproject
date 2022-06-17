96296ea4d994fa8ef935d52775536893872e7d43
Error-Min <smgm030@gmail.com>
2022-06-17 10:57:07 +0900

    22-06-17 10:57

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var port = 8001;
+app.listen(port, function() {
+    console.log('App listing as http://localhost:', port);
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/html/index.html');
+})
