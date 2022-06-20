abae94a25501acb62fb6dec178cb27aa6c2b2cd6
Error-Min <smgm030@gmail.com>
2022-06-20 14:42:01 +0900

    22-06-20 var2

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var port = 8001;
+
+/*nodejs 부트스트랩 불러오기*/
+app.use(express.static(path.join(__dirname, 'www')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
+
+/*nodejs 이미지 불러오기*/
+app.use(express.static('static'));
+
+
 app.listen(port, function() {
     console.log('App listing as http://localhost:', port);
 });
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res) { //작업용페이지
     res.sendFile(__dirname + '/static/html/index.html');
 })
