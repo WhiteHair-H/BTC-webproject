74278c28717976f932063da1ac8849937fd0dcca
Error-Min <smgm030@gmail.com>
2022-06-21 13:43:09 +0900

    22-06-21

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app = express();
 var path = require('path');
 var port = 8001;
 
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 /*nodejs 부트스트랩 불러오기*/
 app.use(express.static(path.join(__dirname, 'www')));
@@ -10,13 +16,17 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 
 app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
 
 /*nodejs 이미지 불러오기*/
-app.use(express.static('static'));
+//app.use(express.static('static'));
 
+/*ajax start*/
+let links = require('./models/link'); //..............ajax링크 파일 모듈 연결
+app.use(express.static('public')); //.................클라이언트의 연결 요청이 들어오면 public 폴더에서 index.html 파일을 찾아 클라이언트한테 줌.
+app.get('/:id', function(req, res) { //...............(경로, 함수) 경로로 get 요청이 들어오면 함수를 실행
+    let id = req.params.id; //........................동적요청 요청에 대한 파라미터 request 데이터를 id에 대입.
+    res.sendFile(path.join(__dirname, links[id])); // snedfile를 이용해서 id를 보냄( links 객체 내용 )
+});
+/*ajax end*/
 
 app.listen(port, function() {
     console.log('App listing as http://localhost:', port);
 });
-
-app.get("/", function(req, res) { //작업용페이지
-    res.sendFile(__dirname + '/static/html/index.html');
-})
---
+++ models/link.js
@@ -0,0 +1,7 @@
+const links = {
+    home: "public/index.html",
+    login: "public/html/login.html"
+}
+
+module.exports = links;
+exports.links = links; // 모듈 외부로 공개할 이름 .links / 모듈 내부에서의 이름.
