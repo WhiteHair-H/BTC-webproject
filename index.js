47cff57fad819925a9d2e46a54d51e4b82df6df8
dana <dana@example.com>
2022-06-30 17:17:00 +0900

    페이지 구성까지 완료

--- index.js
+++ index.js
@@ -21,34 +21,39 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap',
 /*ajax start*/
 let links = require('./models/link'); //..............ajax링크 파일 모듈 연결 require가 모듈을 포함하는 함수이다.
 app.use(express.static('public')); //.................클라이언트의 연결 요청이 들어오면 public 폴더에서 index.html 파일을 찾아 클라이언트한테 줌.
-app.get('/:id', function(req, res) { //...............(경로, 함수) 경로로 get 요청이 들어오면 함수를 실행
+app.get('/:id', function (req, res) { //...............(경로, 함수) 경로로 get 요청이 들어오면 함수를 실행
     let id = req.params.id; //........................동적요청 요청에 대한 파라미터 request 데이터를 id에 대입.
     res.sendFile(path.join(__dirname, links[id])); // snedfile를 이용해서 id를 보냄( links 객체 내용 )
 });
 /*ajax end*/
 
 
-
-app.post('/dbinsert_post', function(req, res) {
+// DB Insert
+app.post('/dbinsert_post', function (req, res) {
     var name = req.body.inputName;
     var mail = req.body.inputEmail3;
     var phone = req.body.inputPhone;
     var pass = req.body.inputPassword3;
     var com = req.body.gridRadios;
 
+    if (!name || !mail || !phone || !pass) {
+        res.write("<script>alert('모든 정보를 입력해주세요.')</script>")
+        return res.write("<script>window.location=\"/login\"</script>");
+    }
+
     var insert = `insert into users values ('${name}','${mail}','${phone}','${pass}','${com}');`
 
     var mysql = require('mysql');
     var connection = mysql.createConnection({
         host: 'localhost',
-        user: 'SANGMIN',
+        user: 'dana',
         password: '1234',
-        database: 'BTCWEB'
+        database: 'testdb'
     });
 
     connection.connect();
 
-    connection.query(insert, function(error, results, fields) {
+    connection.query(insert, function (error, results, fields) {
         if (error) {
             console.log(error);
         }
@@ -57,11 +62,35 @@ app.post('/dbinsert_post', function(req, res) {
 
     connection.end();
 
-    res.render('mem.ejs', { 'name': req.body.inputName });
+    //res.render('mem.ejs', { 'name': req.body.inputName });
+    res.write("<script>alert('success')</script>");
+    res.write("<script>window.location=\"/login\"</script>");
 })
 
 
+// login
+// app.post('/login', function(req,res){
 
-app.listen(port, function() {
+//     var email = req.body.inputEmail3;
+//     var pass = req.body.inputPassword3;
+    
+//     var mysql = require('mysql');
+//     var connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'dana',
+//         password: '1234',
+//         database: 'testdb'
+//     });
+
+//     //connection.
+    
+    
+// })
+
+   
+
+
+
+app.listen(port, function () {
     console.log('App listing as http://localhost:', port);
 });
--- models/link.js
+++ models/link.js
@@ -1,7 +1,22 @@
+// 링크를 통한 각 메뉴들 html 경로
 const links = {
     home: "public/index.html",
-    login: "public/html/login.html",
-    membership: "public/html/membership.html",
+    login: "views/login.html",
+    membership: "views/membership.html",
+    about: "views/about.html",
+    recruit: "views/recruit.html",
+    process: "views/process.html",
+    reference: "views/reference.html",
+    news: "views/news.html",
+
+    /* 서비스 및 제품 서비스 */
+    services_1: "views/service_product/services_1.html",
+    services_2: "views/service_product/services_2.html",
+    services_3: "views/service_product/services_3.html",
+    services_4: "views/service_product/services_4.html",
+    services_5: "views/service_product/services_5.html",
+    services_6: "views/service_product/services_6.html",
+
 }
 
 module.exports = links;
