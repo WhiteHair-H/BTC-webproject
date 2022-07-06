bd72ce96a1f3491104caf4b8a02db5252e4b1dc4
dana <dana@example.com>
2022-07-06 12:00:06 +0900

    회원가입 일치여부 완료

--- index.js
+++ index.js
@@ -21,41 +21,43 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap',
 
 /*ajax start*/
 let links = require('./models/link'); //..............ajax링크 파일 모듈 연결 require가 모듈을 포함하는 함수이다.
+const { Router } = require('express');
+//const { Client } = require('socket.io/dist/client');
 //const { Client } = require('socket.io/dist/client');
 app.use(express.static('public')); //.................클라이언트의 연결 요청이 들어오면 public 폴더에서 index.html 파일을 찾아 클라이언트한테 줌.
 app.get('/:id', function (req, res) { //...............(경로, 함수) 경로로 get 요청이 들어오면 함수를 실행
     let id = req.params.id; //........................동적요청 요청에 대한 파라미터 request 데이터를 id에 대입.
 
     // views/login.html을 입력하지 않을 경우 path 타입이 string으로 보내야한다는 오류가 발생함.
-    res.sendFile(path.join(__dirname, links[id]),'/views/login.html'); // sendfile를 이용해서 id를 보내고 login.html 파일 요청
+    res.sendFile(path.join(__dirname, links[id]), "/public/index.html"); // sendfile를 이용해서 id를 보내고 login.html 파일 요청
 });
 /*ajax end*/
 
 
+// 회원가입
 // DB Insert
+// TODO : DB Data와 Input 데이터의 일치값을 비교하는 쿼리문 필요
+// TODO : 비밀번호 확인 함수
 app.post('/dbinsert_post', function (req, res) {
+    // html name 값을 변수에 삽입
     var name = req.body.inputName;
     var mail = req.body.inputEmail3;
     var phone = req.body.inputPhone;
     var pass = req.body.inputPassword3;
     var com = req.body.gridRadios;
 
-    console.log(name,phone)
-
     // 모든 정보를 입력하지 않았을 때 메시지 박스 띄우기
     if (!name || !mail || !phone || !pass) {
 
         // 한글 깨짐 방지
-        res.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
         // 메시지 박스 띄움
         res.write("<script>alert('모든 정보를 입력해주세요.')</script>")
         // 메시지 박스가 띄워지고 나서 제자리로 돌아감
-        return res.write("<script>window.location=\"/login\"</script>");
+        return res.write("<script>window.location=\"/membership\"</script>");
     }
 
-    // DB insert
-    var insert = `insert into users values ('${name}','${mail}','${phone}','${pass}','${com}');`
-
+    // DB 커넥션 생성후 자신이 생성한 호스트, 유저, 비번, DB 입력
     var mysql = require('mysql');
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -64,30 +66,46 @@ app.post('/dbinsert_post', function (req, res) {
         database: 'testdb'
     });
 
+    // DB 커넥션 연결
     connection.connect();
 
-    connection.query(insert, function (error, results, fields) {
-        if (error) {
-            console.log(error);
+    // DB insert 구문 변수 저장
+    var insert = `insert into users values ('${name}','${mail}','${phone}','${pass}','${com}');`
+
+    connection.query('SELECT * FROM users WHERE u_mail = ?', [mail], function (error, results, fields) {
+        if (results.length == 0) {
+            // 한글 깨짐 방지
+            res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+            res.write("<script>alert('회원가입성공')</script>");
+
+            // DB 쿼리 작성과 에러가 발생하면 에러 로그 출력
+            connection.query(insert, function (error, results, fields) {
+                if (error) {console.log(error);} 
+            });
+            res.write("<script>window.location=\"/login\"</script>");
+
+            if (error) {console.log(error);}
+            
+        }else{
+            res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+            res.write("<script>alert('회원가입실패')</script>");
+            res.write("<script>window.location=\"/membership\"</script>");
         }
-        console.log('The solution is: ', results);
-    });
-
-    connection.end();
-
-    //res.render('mem.ejs', { 'name': req.body.inputName });
-    res.write("<script>alert('success')</script>");
-    res.write("<script>window.location=\"/login\"</script>");
+    })
 })
 
 
-// login
-app.post('/login_post', function(req,res){
+// 로그인
+// DB Select
+app.post('/login_post', function (req, res) {
+    // 이메일과 비번 name 변수값 저장
     var mail = req.body.inputEmail3;
     var pass = req.body.inputPassword3;
 
+    // 이메일과 비번 입력값 출력(입력값이 제대로 출력되는 테스트)
     console.log(mail, pass)
 
+    // DB 커넥션 생성
     var mysql = require('mysql');
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -96,10 +114,15 @@ app.post('/login_post', function(req,res){
         database: 'testdb'
     });
 
+    // DB 커넥션 연결
     connection.connect();
 
+    // 입력값의 메일과 비번을 비교하여 로그인이 될 수 있게 만드는 구문
+    // 만약 메일과 비번이 DB 조회를 통해 입력값과 비교를 하고
+    // 에러가 발생하면 에러 로그를 발생시킨 뒤
+    // 결과값의 길이가 0보다 클 경우 로그인이 되도록 구성
     if (mail && pass) {
-        connection.query('SELECT * FROM users WHERE u_mail = ? AND u_password = ?', [mail, pass], function(error, results, fields) {
+        connection.query('SELECT * FROM users WHERE u_mail = ? AND u_password = ?', [mail, pass], function (error, results, fields) {
             if (error) throw error;
             if (results.length > 0) {
                 //req.session.loggedin = true;
@@ -108,12 +131,12 @@ app.post('/login_post', function(req,res){
                 //res.send("<script>alert('로그인되었습니다.')</script>");
                 res.send('<script type="text/javascript">alert("로그인 되었습니다."); document.location.href="/";</script>');
                 res.end();
-            } else {              
-                res.send('<script type="text/javascript">alert("로그인 정보가 일치하지 않습니다."); document.location.href="/login";</script>');    
-            }            
+            } else {
+                res.send('<script type="text/javascript">alert("로그인 정보가 일치하지 않습니다."); document.location.href="/login";</script>');
+            }
         });
-    } else {        
-        res.send('<script type="text/javascript">alert("username과 password를 입력하세요!"); document.location.href="/login";</script>');    
+    } else {
+        res.send('<script type="text/javascript">alert("email과 password를 입력하세요!"); document.location.href="/login";</script>');
         res.end();
     }
 })
